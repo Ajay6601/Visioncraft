8af2e005bbc9778e03ad16aef4c19191ae73ea46
Ajay6601 <66854965+Ajay6601@users.noreply.github.com>
2025-01-22 15:20:39 -0500

    update Home page
    page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home() {
   return (
           <div>
             <h2>
-              Hello
+              Hello Welcome to VisionCraft
             </h2>
+            <Button variant="destructive">Subscription</Button>
+          
+          
+          
           </div>
     
   );

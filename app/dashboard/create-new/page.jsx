a243dc15bcde6a1afe2a24e4013a03d25217f3a9
Ajay6601 <66854965+Ajay6601@users.noreply.github.com>
2025-01-24 12:53:56 -0500

    Dashboard page update

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function CreateNew() {
+
   return (
-    <div>CreateNew</div>
+    <div className='md:px-20'>
+      <h2 className='font-bold text-4xl text-primary text-center'></h2>
+    <div className='mt-10 shadow-md p-10'>
+    {/* */}
+    <SelectTopic/>
+    </div>
+    </div>
+
   )
 }
 
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,15 +2,20 @@
 import React, { useState } from "react";
 import {Button} from '@/components/ui/button'
 import EmptyState from "./_components/EmptyState";
+import Link from "next/link";
+
 function Dashboard() {
   const [videoList,setVideoList]=useState([]);
   return (
   <div>
     <div className="flex justify-between items-center">
       <h2 className="font-bold text-2xl text-primary">Dashboard</h2>
+
+      <Link href={'/dashboard/create-new'}>
       <Button>
         + Create New
       </Button>
+      </Link>
     </div>
     {/* Empty State */}
     {videoList?.length==0&& <div>

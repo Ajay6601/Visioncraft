19ed6fdf12e706724533659cbf662cb4c2627d38
Ajay6601 <66854965+Ajay6601@users.noreply.github.com>
2025-02-07 12:44:44 -0500

    saving videos data

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import SelectTopic from './_components/SelectTopic'
 import SelectStyle from './_components/SelectStyle';
 import SelectDuration from './_components/SelectDuration';
@@ -7,21 +7,7 @@ import { Button } from '@/components/ui/button';
 import axios from 'axios';
 import CustomLoading from './_components/CustomLoading';
 import {v4 as uuidv4} from 'uuid';
-
-
-const scriptData="In the dead of night, the hospital's corridors stretched into a realm of forgotten dread. A lone wheelchair stood abandoned, its silent wheels whispering forgotten tales. From the shadows, a pair of feet shuffled forward, their presence disturbing the silence A spectral figure materialized at the end of the hall, its form wavering and chilling. The corridor was silent once more, but the echoes of terror still lingered in the darkness."
-const FILE_URL="https://firebasestorage.googleapis.com/v0/b/visioncraft-b335b.firebasestorage.app/o/visioncraft%2F718ab756-210f-4f8d-ae80-6eb5359b7078.mp3?alt=media&token=686439ca-5788-42a2-ab0e-b3fec3ab2b3c"
-const VideoSCRIPT=[
-  {
-    "imagePrompt": "A bustling marketplace in ancient Rome, with people selling goods, merchants haggling, and citizens going about their daily lives. Realistic, detailed, and vibrant colors.",
-    "contentText": "Welcome to ancient Rome, a city buzzing with life and commerce. Imagine a bustling marketplace, filled with the cries of vendors and the chatter of citizens."
-  },
-  {
-    "imagePrompt": "A portrait of a Roman senator, stern and powerful, wearing a toga and sitting on marble throne. Realistic, with sharp details and a sense of authority.",
-    "contentText": "Among them, a powerful senator named Cato the Younger, known for his strong principles and unwavering patriotism."
-  }
-]
-
+import { VideoDataContext } from '@/app/_context/VideoDataContext';
 
 
 function CreateNew() {
@@ -31,6 +17,9 @@ function CreateNew() {
   const [audioFileUrl,setAudioFileUrl]=useState();
   const [imageList,setImageList]=useState();
   const [captions,setCaptions]=useState();
+  const {videoData,setVideoData}=useContext(VideoDataContext);
+  
+  
   const onHandleInputChange=(fieldName,fieldValue)=>{
     console.log(fieldName,fieldValue)
     setFormData(prev=>({
@@ -39,51 +28,64 @@ function CreateNew() {
   }))
   }
 
+  const onCreateClickHandler=()=>{
+    GetVideoScript();
+  
+  }
   const GetVideoScript=async()=>{
     setLoading(true)
     const prompt='Write a script to generate '+formData.duration+' video on topic : '+formData.topic+' along with AI image prompt in '+formData.imageStyle+' format for each scene and give me result in JSON format with imagePrompt and ContentText as field, No Plain text'
     console.log(prompt)
     const resp= await axios.post('/api/get-video-script',{prompt:prompt});
-    if(resp.data.result.videoScenes){
-      console.log(resp.data.result.videoScenes);
+    // console.log(resp.data.result)
+    if(resp.data.result){
+      setVideoData(
+        prev=>({
+          ...prev,
+          'videoScript':resp.data.result.videoScenes
+        })
+      )
       setVideoScript(resp.data.result.videoScenes);
       await GenerateAudioFile(resp.data.result.videoScenes)
     }
   }
 
 
-  const onCreateClickHandler=()=>{
-    GetVideoScript();
-    // GenerateAudioFile(scriptData);
-    // GenerateAudioCaption(FILE_URL);
-    // GenerateImage();
-  }
-
   const GenerateAudioFile=async(videoScriptData)=>{
     setLoading(true)
     let script='';
     const id=uuidv4();
+    console.log(videoScriptData);
     videoScriptData.forEach(item=>{
       script=script+item.contentText+' ';
     })
     const resp=await axios.post('/api/generate-audio',{
       text:script,
       id:id 
-    });
-      setAudioFileUrl(resp.data.result);
-      resp.data.result&&await GenerateAudioCaption(resp.data.result,videoScriptData)
-      
+    });    
+    setVideoData(
+      prev=>({
+        ...prev,
+        'audioFileUrl':resp.data.Result
+      })
+    )
+      setAudioFileUrl(resp.data.Result);
+      resp.data.Result && await GenerateAudioCaption(resp.data.Result,videoScriptData)
   }
 
   const GenerateAudioCaption=async(fileUrl,videoScriptData)=>{
     setLoading(true);
     const resp=await axios.post('/api/generate-caption',{
       audioFileUrl:fileUrl
-    });
-    setCaptions(resp?.data?.result)
-      console.log(resp.data.result)
+    })
+    setCaptions(resp?.data?.Result)
+    setVideoData(
+      prev=>({
+        ...prev,
+        'captions':resp.data.result
+      })
+    )
       resp.data.result&& await GenerateImage(videoScriptData);
- 
     }
   
     const GenerateImage=async(videoScriptData)=>{
@@ -91,20 +93,28 @@ function CreateNew() {
       for(const element of videoScriptData)
       {
         try{
-          const resp=await axios.post('/api/generate-image',{
-            prompt:element.imagePrompt
-          });
+          const resp=await axios.post('/api/generate-image',{prompt:element.imagePrompt});
           console.log(resp.data.result);
           images.push(resp.data.result);
-
         }catch(e)
         {
           console.log('Error:'+e);
         }
       }
+      setVideoData(
+        prev=>({
+          ...prev,
+          'imageList':images
+        })
+      )
       setImageList(images)
       setLoading(false);
     }
+
+    useEffect(()=>{
+      console.log(videoData);
+    },[videoData])
+
   return (
     <div className='md:px-20'>
       <h2 className='font-bold text-4xl text-primary text-center'></h2>
